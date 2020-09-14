012563e1a3546270abcb90636112db61d96c5f0f
unknown <5c0tt.411en@gmail.com>
2020-09-14 21:58:39 +0900

    updated draw vertex

--- 03_advanved_course/02_drawVertex/sketch.js
+++ 03_advanved_course/02_drawVertex/sketch.js
@@ -1,9 +1,21 @@
 function setup() {
     createCanvas(400, 400);
+    blendMode(ADD);
+    colorMode(HSB);
 }
  
 function draw() {
     background(0);
-    fill(255);
-    circle(width / 2, height / 2, 100);
+
+    for (let j = 0; j < 15; j++) {
+        // stroke(255);
+        fill(random(50, 360), 100, 100, 0.4);
+        beginShape();
+        for (let i = 2; i <= 10; i+=2) {
+            vertex(width / 2 + (random() < 0.5 ? width / i : - width / i),
+                height / 2 + (random() < 0.5 ? height / i : - height / i));
+        }        
+        endShape();
+    }
+    noLoop();
 }
