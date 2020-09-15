a0c4777d3b1a537206d88ba8412b27f7c838b377
unknown <5c0tt.411en@gmail.com>
2020-09-15 14:24:02 +0900

    updated mix color

--- 03_advanved_course/04_mixColor/sketch.js
+++ 03_advanved_course/04_mixColor/sketch.js
@@ -5,17 +5,18 @@ let x, y;
 function setup() {
     createCanvas(400, 400);
     noFill();
-    strokeWeight(2);
+    strokeWeight(1);
+    blendMode(ADD);
 }
  
 function draw() {
     background(0);
     for (let j = 0; j < n; j++) { 
-        let from = color(random(150, 180), random(150, 255), random(150, 255));
-        let to = color(40, random(20, 150), random(100, 200));
+        let from = color(random(150, 180), random(150, 255), random(150, 255), 50);
+        let to = color(40, random(20, 150), random(100, 200), 20);
         x = random(width);
         y = random(height);
-        gradientStep = random(10, 80);
+        gradientStep = random(10, 150);
         for (let i = 0; i < gradientStep; i++) { 
             stroke(lerpColor(from, to,  i / gradientStep));
             circle(x, y, i);
